bfe5ef81055443dd146e348d0cc01a1a17d88214
godsdog <an.andrusyk@gmail.com>
2018-03-24 23:34:35 +0200

    Add 'polls' model

--- django_try/settings.py
+++ django_try/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,6 +77,7 @@ WSGI_APPLICATION = 'django_try.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
@@ -106,7 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
--- readme.txt
+++ readme.txt
@@ -1,4 +1,8 @@
 > python3 -m venv ~/VIRTUALENV/django
 > python manage.py makemigrations
+> # python manage.py sqlmigrate polls 0001
+> #  python manage.py check
 > python manage.py migrate
 > python manage.py runserver 8000
+
+> python manage.py shell
